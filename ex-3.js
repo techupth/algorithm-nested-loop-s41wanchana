ae43a1a78901374657c28c6518a849e5291e6fa9
Wanchana <s.wanchana41@gmail.com>
2024-07-03 11:56:24 +0700

    feat: exercise 3

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n; j++) {
+      row += j * i + "\t";
+    }
+    result += row + "\n";
+  }
+  return result;
 }
+
+console.log(multiplicationTable(5));
