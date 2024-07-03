7464c1b76dab1be2e437651ad5b0c6032f58d9df
Wanchana <s.wanchana41@gmail.com>
2024-07-03 11:31:48 +0700

    feat: exercise 1

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,14 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += j + " ";
+    }
+    result += row + "\n";
+  }
+  return result;
 }
+
+console.log(printPattern(5));
